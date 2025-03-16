0abf75cb1579a4ca948cc185cc26862f060f87a9
Abd elaziz Soltani <abz.soltani@gmail.com>
2025-03-16 07:25:38 +0100

    save before implementing beacon activity branch

--- app/src/main/java/com/example/testappqr/MainActivity.kt
+++ app/src/main/java/com/example/testappqr/MainActivity.kt
@@ -1,64 +1,35 @@
 
 package com.example.testappqr
 
-import android.content.Intent
-import android.os.Build
 import android.os.Bundle
-import android.widget.Toast
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.appcompat.app.AppCompatActivity
 import com.example.testappqr.theme.EmargementTheme
 import com.example.testappqr.data.models.SSODTO
 import com.example.testappqr.data.models.Attributes
 import com.example.testappqr.data.models.AuthenticationSuccess
 import com.example.testappqr.presentation.navigation.NavGraph
-import com.example.testappqr.presentation.student.views.BeaconScannerService
 import dagger.hilt.android.AndroidEntryPoint
 
 import java.util.UUID
 
-//
-//class MainActivity : ComponentActivity() {
-//    override fun onCreate(savedInstanceState: Bundle?) {
-//        super.onCreate(savedInstanceState)
-//        setContent {
-//            EmargementTheme {
-//                NavGraph(SharedModel())
-//            }
-//        }
-//    }
-//}
 @AndroidEntryPoint
-class MainActivity : ComponentActivity() {  // Changement d'extension vers AppCompatActivity
+class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-
-//        startBeaconScannerService()
-
         setContent {
             EmargementTheme {
                 NavGraph(SharedModel())
             }
         }
     }
-
-    private fun startBeaconScannerService() {
-        val intent = Intent(this, BeaconScannerService::class.java)
-        if (Build.VERSION.SDK_INT >= Build.VERSION_CODES.O) {
-            startForegroundService(intent)
-        } else {
-            startService(intent)
-        }
-        Toast.makeText(this, "Beacon scanning started", Toast.LENGTH_SHORT).show()
-    }
 }
+
 
 class SharedModel {
     var apiSSOResponse: SSODTO? = SSODTO(
         authenticationSuccess = AuthenticationSuccess(
-            user = "s23022841",
-            attributes = Attributes(
+            user = "s23022841", attributes = Attributes(
                 amuComposante = "sciences",
                 coGroup = "AMU.M2_IMI_CCI-SMI5T1-V302-2024",
                 mail = "abdelaziz.SOLTANI@etu.univ-amu.fr",
@@ -87,6 +58,3 @@ class SharedModel {
     val development: Boolean = true
 
 }
-
-
-
